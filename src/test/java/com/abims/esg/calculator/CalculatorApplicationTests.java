2dfd7969b25885b50a9e65411c0bdb0c63d27774
abimsodipo <abimsodipo@yahoo.co.uk>
2024-02-03 11:30:50 +0000

    Repository setup

---
+++ src/test/java/com/abims/esg/calculator/CalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abims.esg.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
